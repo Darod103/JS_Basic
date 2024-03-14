128a180530404b172e93d7ae953644347005809a
Darod103 <darod103@gmail.com>
2024-03-14 22:24:47 +1000

    refactoring sort-loops

--- 9-sort-loops/app.js
+++ 9-sort-loops/app.js
@@ -1,15 +1,16 @@
 const data = [22,33,10,1,3,4,12,32,16,2,8]
 
 function bubbleSort(arr){
-    for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr.length - 1 - i; j++){
-            if(arr[j]> arr[j+1]){
-                const temp = arr[j];
-                arr[j]= arr[j+1];
-                arr[j+1] = temp;
+    let result = [...arr]
+    for(let i = 0; i < result.length; i++){
+        for(let j = 0; j < result.length - 1 - i; j++){
+            if(result[j]> result[j+1]){
+                const temp = result[j];
+                result[j]= result[j+1];
+                result[j+1] = temp;
             }
         }
 
     }
-    return arr;
+    return result;
 };
